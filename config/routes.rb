48c29a6ca478ee0af52acadd4eb042c9e80b989f
hugokl <hugokl@outlook.com>
2015-09-23 06:07:21 +0000

    Finish user edit, update, index and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -11,59 +11,24 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
 
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+=begin
+Prefix Verb   URI Pattern               Controller#Action
+sessions_new GET    /sessions/new(.:format)   sessions#new
+        root GET    /                         static_pages#home
+        help GET    /help(.:format)           static_pages#help
+       about GET    /about(.:format)          static_pages#about
+     contact GET    /contact(.:format)        static_pages#contact
+      signup GET    /signup(.:format)         users#new
+       login GET    /login(.:format)          sessions#new
+             POST   /login(.:format)          sessions#create
+      logout DELETE /logout(.:format)         sessions#destroy
+       users GET    /users(.:format)          users#index
+             POST   /users(.:format)          users#create
+    new_user GET    /users/new(.:format)      users#new
+   edit_user GET    /users/:id/edit(.:format) users#edit
+        user GET    /users/:id(.:format)      users#show
+             PATCH  /users/:id(.:format)      users#update
+             PUT    /users/:id(.:format)      users#update
+             DELETE /users/:id(.:format)      users#destroy
+=end
 end
